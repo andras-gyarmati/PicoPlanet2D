86d4a2a47b77f6ed632dddb4894ba2c9d96d40cc
andras-gyarmati <bandieee16@gmail.com>
2019-02-25 23:53:25 +0100

    Add jump, partially fixed gravity

--- PicoPlanet/Assets/Scripts/Planet.cs
+++ PicoPlanet/Assets/Scripts/Planet.cs
@@ -1,17 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    public float gravity = -10f;
+    public float GravitationalConstant;
 
-    public void Attract(Transform body)
+    public void Attract(Transform other)
     {
-        Vector2 gravityUp = (body.position - transform.position).normalized;
-        Vector2 bodyUp = body.up;
-        body.GetComponent<Rigidbody2D>().AddForce(gravityUp * gravity);
-        Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * body.rotation;
-        body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 50 * Time.deltaTime);
+        Rigidbody2D otherRb = other.GetComponent<Rigidbody2D>();
+        Rigidbody2D rb = transform.GetComponent<Rigidbody2D>();
+        Vector2 gravityUp = (other.position - transform.position).normalized;
+        Vector2 bodyUp = other.up;
+        float gravity = ((GravitationalConstant * rb.mass * otherRb.mass) / Vector2.Distance(other.position, transform.position));
+        otherRb.AddForce(gravityUp * -gravity);
+        Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * other.rotation;
+        other.rotation = Quaternion.Slerp(other.rotation, targetRotation, 50 * Time.deltaTime);
     }
 }
--- PicoPlanet/Assets/Scripts/PlayerControl.cs
+++ PicoPlanet/Assets/Scripts/PlayerControl.cs
@@ -2,16 +2,48 @@
 
 public class PlayerControl : MonoBehaviour
 {
-    public float speed = 10f;
+    public float speed;
+    public float jumpForce;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
     private Vector2 direction;
+    private Rigidbody2D rb;
+    private bool facingRight;
+    private bool isGrounded;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        facingRight = true;
+    }
 
     private void Update()
     {
         direction = new Vector2(Input.GetAxis("Horizontal"), 0);
     }
 
-    private void FixedUpdate() {
-        var rb = GetComponent<Rigidbody2D>();
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        direction = transform.TransformDirection(direction);
         rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
+        if ((!facingRight && direction.x > 0) || (facingRight && direction.x < 0))
+        {
+            Flip();
+        }
+        if (isGrounded && (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Jump")))
+        {
+            rb.AddForce(transform.up * jumpForce);
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
     }
 }
